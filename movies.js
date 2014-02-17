bcc11ca48172cb97da3edb6dd8a8869d9c751c93
hlopez <hlopezm23@gmail.com>
2014-02-17 15:55:08 +0100

    My first commit Javascript movies practice

---
+++ movies.js
@@ -0,0 +1,5 @@
+var guardarLocal = function() {
+
+}
+
+var movies = [{"nombre_peli": "Gladiator", "año": "1985", "genero": "Historica"}, {"nombre_peli": "Titanic", "año": "2000", "genero": "Drama"}];
